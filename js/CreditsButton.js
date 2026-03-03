fd0b46de39c66bccdfb89508d302cec0ad8ae97d
ICAlejandro <fishdeshu@gmail.com>
2026-03-03 17:39:57 +0800

    added Credits feature
    credits button and placeholder credit information

---
+++ js/CreditsButton.js
@@ -0,0 +1,31 @@
+class CreditsModal {
+  constructor() {
+    this.overlay  = document.getElementById('creditsModal');
+    this.closeBtn = document.getElementById('creditsModalClose');
+
+    this._bindEvents();
+  }
+
+  /** Open Credits */
+  open() {
+    this.overlay.classList.add('is-open');
+  }
+
+  close() {
+    this.overlay.classList.remove('is-open');
+  }
+
+  isOpen() {
+    return this.overlay.classList.contains('is-open');
+  }
+
+  _bindEvents() {
+    this.closeBtn.addEventListener('click', () => this.close());
+    this.overlay.addEventListener('click', (e) => {
+      if (e.target === this.overlay) this.close();
+    });
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' && this.isOpen()) this.close();
+    });
+  }
+}
